ebb146d6686fc05de1df1f7569d75df9f953e33f
AlaskanEmily <YoloShart@gmail.com>
2018-02-18 23:59:56 -0800

    Refactor makefiles

--- cin_build_dsound.mk
+++ cin_build_dsound.mk
@@ -2,3 +2,5 @@
 # http://creativecommons.org/publicdomain/zero/1.0/
 
 DSOUND_OBJECTS=cin_driver_dsound$(OBJ) cin_loader_soft$(OBJ) cin_sound_dsound$(OBJ)
+CIN_DRIVER_DSOUND_DEPS=$(COMMON_DSOUND_DEPS) cin_driver_dsound.cpp cin_driver.h
+CIN_SOUND_DSOUND_DEPS=$(COMMON_DSOUND_DEPS) cin_sound_dsound.cpp cin_sound.h cin_loader.h cin_loader_soft.h
