8368a5a0fccc8b6684c57b3718eec6b8e3301639
AlaskanEmily <yoloshart@gmail.com>
2018-05-26 21:52:14 -0700

    Fix OpenAL build

--- openal/cin_build_openal.mk
+++ openal/cin_build_openal.mk
@@ -1,7 +1,7 @@
 # Any copyright is dedicated to the Public Domain.
 # http://creativecommons.org/publicdomain/zero/1.0/
 
-OPENAL_OBJECTS=openal$(PATHSEP)cin_driver_openal$(OBJ) cin_loader_openal$(OBJ) cin_sound_openal$(OBJ)
+OPENAL_OBJECTS=cin_driver_openal$(OBJ) cin_loader_openal$(OBJ) cin_sound_openal$(OBJ)
 COMMON_OPENAL_DEPS=openal$(PATHSEP)cin_openal.h cin_loader.h $(COMMON_DEPS)
 CIN_LOADER_OPENAL_DEPS=$(COMMON_OPENAL_DEPS) openal$(PATHSEP)cin_loader_openal.c
 CIN_DRIVER_OPENAL_DEPS=$(COMMON_OPENAL_DEPS) openal$(PATHSEP)cin_driver_openal.c cin_driver.h
