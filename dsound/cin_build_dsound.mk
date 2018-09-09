f1282fa33ce75a0d07f1deb13bb4ad8cedf8cb78
AlaskanEmily <emily@alaskanemily.net>
2018-09-08 17:14:01 -0700

    Fix DirectSound dependencies

--- dsound/cin_build_dsound.mk
+++ dsound/cin_build_dsound.mk
@@ -2,5 +2,5 @@
 # http://creativecommons.org/publicdomain/zero/1.0/
 
 DSOUND_OBJECTS=cin_driver_dsound$(OBJ) cin_loader_soft$(OBJ) cin_sound_dsound$(OBJ) cin_lock_$(MIXER_TYPE)$(OBJ) cin_thread_$(MIXER_TYPE)$(OBJ) cin_driver_mixer$(OBJ)
-CIN_DRIVER_DSOUND_DEPS=$(COMMON_DSOUND_DEPS) dsound$(PATHSEP)cin_driver_dsound.cpp cin_driver.h mixer$(PATHSEP)cin_lock.h mixer$(PATHSEP)cin_thread.h
-CIN_SOUND_DSOUND_DEPS=$(COMMON_DSOUND_DEPS) dsound$(PATHSEP)cin_sound_dsound.cpp cin_sound.h cin_loader.h cin_loader_soft.h
+CIN_DRIVER_DSOUND_DEPS=$(COMMON_DSOUND_DEPS) dsound$(PATHSEP)cin_driver_dsound.cpp dsound$(PATHSEP)cin_driver_dsound.hpp cin_driver.h mixer$(PATHSEP)cin_lock.h mixer$(PATHSEP)cin_thread.h
+CIN_SOUND_DSOUND_DEPS=$(COMMON_DSOUND_DEPS) dsound$(PATHSEP)cin_sound_dsound.cpp dsound$(PATHSEP)cin_sound_dsound.hpp cin_sound.h cin_loader.h cin_loader_soft.h
